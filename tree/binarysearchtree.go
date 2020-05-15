6415d2f2b04b66bc473344b856b5926f56b3c1c6
yuheng chen <dunkee182@gmail.com>
2020-05-15 16:56:47 +0800

    basic trees

---
+++ tree/binarysearchtree.go
@@ -0,0 +1,44 @@
+package binarysearchtree
+
+import "fmt"
+
+type node struct {
+	Value int
+	Left  *node
+	Right *node
+}
+
+func insert(n **node, v int) {
+	if *n == nil {
+		*n = &node{Value: v, Left: nil, Right: nil}
+	} else if v < (*n).Value {
+		insert(&(*n).Left, v)
+	} else {
+		insert(&(*n).Right, v)
+	}
+}
+
+func print(n *node) {
+	if n != nil {
+		print(n.Left)
+		fmt.Println(n.Value)
+		print(n.Right)
+	}
+}
+
+func search(n *node, v int) bool {
+	if n != nil {
+		if n.Value == v {
+			fmt.Print("Got it: ")
+			fmt.Print(n.Value)
+			fmt.Print(" ")
+			return true
+		} else if v < n.Value {
+			return search(n.Left, v)
+		} else {
+			return search(n.Right, v)
+		}
+	}
+
+	return false
+}
---
+++ tree/binarysearchtree_test.go
@@ -0,0 +1,33 @@
+package binarysearchtree
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBinarySearchTree(t *testing.T) {
+	var root *node = nil
+
+	insert(&root, 8)
+	insert(&root, 10)
+	insert(&root, 4)
+	insert(&root, 2)
+	insert(&root, 6)
+	insert(&root, 20)
+	insert(&root, 3)
+	insert(&root, 5)
+	insert(&root, 2)
+	insert(&root, -10)
+	insert(&root, -20)
+
+	fmt.Println(search(root, 6))
+	fmt.Println(search(root, 20))
+	fmt.Println(search(root, -10))
+	fmt.Println(search(root, 8))
+	fmt.Println(search(root, 3))
+	fmt.Println(search(root, 30))
+	fmt.Println(search(root, 17))
+	fmt.Println(search(root, 0))
+	fmt.Println(search(root, 1))
+
+}
