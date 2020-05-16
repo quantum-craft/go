91f8c89881feef1cf6d436408c74e394bf62360f
yuheng chen <dunkee182@gmail.com>
2020-05-16 23:01:46 +0800

    finish minheap

--- minheap/minheap.go
+++ minheap/minheap.go
@@ -5,7 +5,7 @@ import "github.com/quantum-craft/go/dijkstra"
 // Node is node unit for Dijkstra algorithm
 type Node struct {
 	Key   *dijkstra.Vertex
-	Value int // Dijkstra's greedy score
+	Score int // Dijkstra's greedy score
 }
 
 // MinHeap keeps minimum member on the top and keeps the heap property
@@ -28,15 +28,82 @@ func Insert(minheap MinHeap, n Node) {
 	}
 
 	// pos is one based
-	for pos := *lastEmpty; pos > 1 && (*heap)[pos/2-1].Value >= (*heap)[pos-1].Value; pos = pos / 2 {
-		n := (*heap)[pos-1]
-		(*heap)[pos-1] = (*heap)[pos/2-1]
-		(*heap)[pos/2-1] = n
+	for pos := *lastEmpty; pos > 1 && (*heap)[pos/2-1].Score >= (*heap)[pos-1].Score; pos = pos / 2 {
+		swapNode(heap, pos-1, pos/2-1)
 	}
 }
 
 // ExtractMin will extract the minimum member, replace the minimum pos with the last member,
 // and bubble down it to the proper position
-func ExtractMin(heap *[]Node) {
+func ExtractMin(minheap MinHeap) Node {
+	var lastEmpty *int = minheap.lastEmpty
 
+	if *lastEmpty == 0 {
+		return Node{Key: nil, Score: -1}
+	}
+
+	var heap *[]Node = minheap.heap
+	var ret Node = (*heap)[0]
+
+	*lastEmpty--
+	swapNode(heap, 0, *lastEmpty)
+
+	// pos is one based
+	pos := 1
+	for {
+		if pos-1 >= *lastEmpty || pos*2-1 >= *lastEmpty {
+			return ret
+		}
+
+		minPos := -1
+		if pos*2 >= *lastEmpty {
+			minPos = findMinScorePos2(heap, pos, pos*2)
+		} else {
+			minPos = findMinScorePos3(heap, pos, pos*2, pos*2+1)
+		}
+
+		if minPos == pos {
+			return ret
+		}
+
+		swapNode(heap, pos-1, minPos-1)
+
+		pos = minPos
+	}
+}
+
+// pos is one based
+func findMinScorePos2(heap *[]Node, pos1 int, pos2 int) int {
+	minPos := -1
+
+	if (*heap)[pos1-1].Score < (*heap)[pos2-1].Score {
+		minPos = pos1
+	} else {
+		minPos = pos2
+	}
+
+	return minPos
+}
+
+// pos is one based
+func findMinScorePos3(heap *[]Node, pos1 int, pos2 int, pos3 int) int {
+	minPos := -1
+
+	if (*heap)[pos1-1].Score < (*heap)[pos2-1].Score {
+		minPos = pos1
+	} else {
+		minPos = pos2
+	}
+
+	if (*heap)[pos3-1].Score < (*heap)[minPos-1].Score {
+		minPos = pos3
+	}
+
+	return minPos
+}
+
+func swapNode(heap *[]Node, this int, that int) {
+	n := (*heap)[this]
+	(*heap)[this] = (*heap)[that]
+	(*heap)[that] = n
 }
--- minheap/minheap_test.go
+++ minheap/minheap_test.go
@@ -1,7 +1,15 @@
 package minheap
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
 	"testing"
+
+	"github.com/quantum-craft/go/utils"
 )
 
 func TestMinHeap(t *testing.T) {
@@ -12,11 +20,73 @@ func TestMinHeap(t *testing.T) {
 		lastEmpty: &lastEmpty,
 	}
 
-	Insert(minheap, Node{Key: nil, Value: 88})
-	Insert(minheap, Node{Key: nil, Value: 32})
-	Insert(minheap, Node{Key: nil, Value: 50})
-	Insert(minheap, Node{Key: nil, Value: 90})
-	Insert(minheap, Node{Key: nil, Value: 23})
-	Insert(minheap, Node{Key: nil, Value: 74})
-	Insert(minheap, Node{Key: nil, Value: 2})
+	Insert(minheap, Node{Key: nil, Score: 88})
+	Insert(minheap, Node{Key: nil, Score: 32})
+	Insert(minheap, Node{Key: nil, Score: 50})
+	Insert(minheap, Node{Key: nil, Score: 90})
+	Insert(minheap, Node{Key: nil, Score: 23})
+	Insert(minheap, Node{Key: nil, Score: 74})
+	Insert(minheap, Node{Key: nil, Score: 2})
+
+	fmt.Println(ExtractMin(minheap))
+	fmt.Println(ExtractMin(minheap))
+	fmt.Println(ExtractMin(minheap))
+
+	Insert(minheap, Node{Key: nil, Score: 5})
+	Insert(minheap, Node{Key: nil, Score: 91})
+	Insert(minheap, Node{Key: nil, Score: 13})
+	Insert(minheap, Node{Key: nil, Score: 1})
+
+	for n := ExtractMin(minheap); n.Score != -1; n = ExtractMin(minheap) {
+		fmt.Println(n)
+	}
+}
+
+func TestHeapSortLarge(t *testing.T) {
+	lineEnding := "\n"
+
+	if runtime.GOOS == "windows" {
+		lineEnding = "\r\n"
+	} else {
+		lineEnding = "\n"
+	}
+
+	f, err := os.Open("../data/QuickSortNumbers.txt")
+	if err != nil {
+		fmt.Println("error opening file= ", err)
+		os.Exit(1)
+	}
+
+	r := bufio.NewReader(f)
+	line, err := r.ReadString('\n')
+
+	numbers := make([]int, 0, 0)
+
+	for err == nil {
+		i, _ := strconv.Atoi(strings.TrimSuffix(line, lineEnding))
+
+		numbers = append(numbers, i)
+
+		line, err = r.ReadString('\n')
+	}
+
+	heap := make([]Node, 0, 0)
+	lastEmpty := 0
+	minheap := MinHeap{
+		heap:      &heap,
+		lastEmpty: &lastEmpty,
+	}
+
+	for i := 0; i < len(numbers); i++ {
+		Insert(minheap, Node{Key: nil, Score: numbers[i]})
+	}
+
+	ans := make([]int, 0)
+	for n := ExtractMin(minheap); n.Score != -1; n = ExtractMin(minheap) {
+		ans = append(ans, n.Score)
+	}
+
+	if !utils.SliceIncreasing(ans) {
+		t.Error("HeapSort error !")
+	}
 }
