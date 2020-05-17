8cb1bd0d32abbe289bf76c20e1b0707cdbc470c8
yuheng chen <dunkee182@gmail.com>
2020-05-17 15:43:33 +0800

    start Dijkstra

--- dijkstra/dijkstra.go
+++ dijkstra/dijkstra.go
@@ -10,6 +10,7 @@ import (
 // Vertex is an element of V of a Graph G(V, E)
 type Vertex struct {
 	idx   int
+	Score int // Dijkstra's greedy score
 	Edges []*Edge
 }
 
@@ -17,6 +18,20 @@ type Vertex struct {
 type Edge struct {
 	Head   *Vertex
 	Weight int
+}
+
+// Dijkstra computes shortest path from startIdx to all other reachable nodes
+func Dijkstra(vertices []Vertex, edges []Edge, startIdx int) {
+	heap := make([]Node, 0, 0)
+	lastEmpty := 0
+	minheap := MinHeap{
+		heap:      &heap,
+		lastEmpty: &lastEmpty,
+	}
+
+	vertices[startIdx].Score = 0 // source
+	Insert(minheap, Node{Key: &vertices[startIdx]})
+
 }
 
 // ConstructGraph constructs the undirected, weighted graph for Dijkstra algorithm
@@ -45,13 +60,15 @@ func ConstructGraph(filePath string) ([]Vertex, []Edge) {
 			weight, _ := strconv.Atoi(w[1])
 
 			if vertices[tail].Edges == nil {
-				vertices[tail].Edges = make([]*Edge, 0)
 				vertices[tail].idx = tail
+				vertices[tail].Score = 1000000 // max weight for our application
+				vertices[tail].Edges = make([]*Edge, 0)
 			}
 
 			if vertices[head].Edges == nil {
-				vertices[head].Edges = make([]*Edge, 0)
 				vertices[head].idx = head
+				vertices[head].Score = 1000000 // max weight for our application
+				vertices[head].Edges = make([]*Edge, 0)
 			}
 
 			currentEdges = edges[:len(currentEdges)+1]
@@ -85,4 +102,109 @@ func VertexAndEdgeCountFromFile(filePath string) (int, int) {
 	}
 
 	return lineCnt, edgeCnt
+}
+
+// Node is node unit for Dijkstra algorithm
+type Node struct {
+	Key *Vertex
+}
+
+// MinHeap keeps minimum member on the top and keeps the heap property
+type MinHeap struct {
+	lastEmpty *int // zero based
+	heap      *[]Node
+}
+
+// Insert will insert the node at the bottom and bubble up to the proper position
+func Insert(minheap MinHeap, n Node) {
+	var heap *[]Node = minheap.heap
+	var lastEmpty *int = minheap.lastEmpty
+
+	if *lastEmpty == len(*heap) {
+		*heap = append(*heap, n)
+		*lastEmpty++
+	} else {
+		(*heap)[*lastEmpty] = n
+		*lastEmpty++
+	}
+
+	// pos is one based
+	for pos := *lastEmpty; pos > 1 && (*heap)[pos/2-1].Key.Score >= (*heap)[pos-1].Key.Score; pos = pos / 2 {
+		swapNode(heap, pos-1, pos/2-1)
+	}
+}
+
+// ExtractMin will extract the minimum member, replace the minimum pos with the last member,
+// and bubble down it to the proper position
+func ExtractMin(minheap MinHeap) Node {
+	var lastEmpty *int = minheap.lastEmpty
+
+	if *lastEmpty == 0 {
+		return Node{Key: nil}
+	}
+
+	var heap *[]Node = minheap.heap
+	var ret Node = (*heap)[0]
+
+	*lastEmpty--
+	swapNode(heap, 0, *lastEmpty)
+
+	// pos is one based
+	pos := 1
+	for {
+		if pos-1 >= *lastEmpty || pos*2-1 >= *lastEmpty {
+			return ret
+		}
+
+		minPos := -1
+		if pos*2 >= *lastEmpty {
+			minPos = findMinScorePos2(heap, pos, pos*2)
+		} else {
+			minPos = findMinScorePos3(heap, pos, pos*2, pos*2+1)
+		}
+
+		if minPos == pos {
+			return ret
+		}
+
+		swapNode(heap, pos-1, minPos-1)
+
+		pos = minPos
+	}
+}
+
+// pos is one based
+func findMinScorePos2(heap *[]Node, pos1 int, pos2 int) int {
+	minPos := -1
+
+	if (*heap)[pos1-1].Key.Score < (*heap)[pos2-1].Key.Score {
+		minPos = pos1
+	} else {
+		minPos = pos2
+	}
+
+	return minPos
+}
+
+// pos is one based
+func findMinScorePos3(heap *[]Node, pos1 int, pos2 int, pos3 int) int {
+	minPos := -1
+
+	if (*heap)[pos1-1].Key.Score < (*heap)[pos2-1].Key.Score {
+		minPos = pos1
+	} else {
+		minPos = pos2
+	}
+
+	if (*heap)[pos3-1].Key.Score < (*heap)[minPos-1].Key.Score {
+		minPos = pos3
+	}
+
+	return minPos
+}
+
+func swapNode(heap *[]Node, this int, that int) {
+	n := (*heap)[this]
+	(*heap)[this] = (*heap)[that]
+	(*heap)[that] = n
 }
--- dijkstra/dijkstra_test.go
+++ dijkstra/dijkstra_test.go
@@ -1,9 +1,99 @@
 package dijkstra
 
-import "testing"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/quantum-craft/go/utils"
+)
 
 func TestDijkstra(t *testing.T) {
-	file := "../data/dijkstraData.txt"
+	file := "../data/dijkstraDataSmall.txt"
 
-	ConstructGraph(file)
+	vertices, edges := ConstructGraph(file)
+	Dijkstra(vertices, edges, 0)
+}
+
+func TestMinHeap(t *testing.T) {
+	heap := make([]Node, 0, 0)
+	lastEmpty := 0
+	minheap := MinHeap{
+		heap:      &heap,
+		lastEmpty: &lastEmpty,
+	}
+
+	Insert(minheap, Node{Key: &Vertex{Score: 88}})
+	Insert(minheap, Node{Key: &Vertex{Score: 32}})
+	Insert(minheap, Node{Key: &Vertex{Score: 50}})
+	Insert(minheap, Node{Key: &Vertex{Score: 90}})
+	Insert(minheap, Node{Key: &Vertex{Score: 23}})
+	Insert(minheap, Node{Key: &Vertex{Score: 74}})
+	Insert(minheap, Node{Key: &Vertex{Score: 2}})
+
+	fmt.Println(ExtractMin(minheap).Key.Score)
+	fmt.Println(ExtractMin(minheap).Key.Score)
+	fmt.Println(ExtractMin(minheap).Key.Score)
+
+	Insert(minheap, Node{Key: &Vertex{Score: 5}})
+	Insert(minheap, Node{Key: &Vertex{Score: 91}})
+	Insert(minheap, Node{Key: &Vertex{Score: 13}})
+	Insert(minheap, Node{Key: &Vertex{Score: 1}})
+
+	for n := ExtractMin(minheap); n.Key != nil; n = ExtractMin(minheap) {
+		fmt.Println(n.Key.Score)
+	}
+}
+
+func TestHeapSortLarge(t *testing.T) {
+	lineEnding := "\n"
+
+	if runtime.GOOS == "windows" {
+		lineEnding = "\r\n"
+	} else {
+		lineEnding = "\n"
+	}
+
+	f, err := os.Open("../data/QuickSortNumbers.txt")
+	if err != nil {
+		fmt.Println("error opening file= ", err)
+		os.Exit(1)
+	}
+
+	r := bufio.NewReader(f)
+	line, err := r.ReadString('\n')
+
+	numbers := make([]int, 0, 0)
+
+	for err == nil {
+		i, _ := strconv.Atoi(strings.TrimSuffix(line, lineEnding))
+
+		numbers = append(numbers, i)
+
+		line, err = r.ReadString('\n')
+	}
+
+	heap := make([]Node, 0, 0)
+	lastEmpty := 0
+	minheap := MinHeap{
+		heap:      &heap,
+		lastEmpty: &lastEmpty,
+	}
+
+	for i := 0; i < len(numbers); i++ {
+		Insert(minheap, Node{Key: &Vertex{Score: numbers[i]}})
+	}
+
+	ans := make([]int, 0)
+	for n := ExtractMin(minheap); n.Key != nil; n = ExtractMin(minheap) {
+		ans = append(ans, n.Key.Score)
+	}
+
+	if !utils.SliceIncreasing(ans) {
+		t.Error("HeapSort error !")
+	}
 }
