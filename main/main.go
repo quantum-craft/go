1f5d0440fe6188334fce26a887fe3120fda8a167
quantum-craft <dunkee182@gmail.com>
2020-12-18 14:34:09 +0800

    update

--- main/main.go
+++ main/main.go
@@ -2,54 +2,70 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
-
-	"github.com/quantum-craft/go/graph"
 )
 
-func report(mincut *int, quitCh chan bool) {
-	for {
-		time.Sleep(2000 * time.Millisecond)
+func minTransfers(transactions [][]int) int {
+	debt := make(map[int]int)
 
-		fmt.Println(*mincut)
-
-		select {
-		case <-quitCh:
-			return
-		default:
-			continue
-		}
+	for i := range transactions {
+		debt[transactions[i][0]] -= transactions[i][2]
+		debt[transactions[i][1]] += transactions[i][2]
 	}
-}
 
-func main2() {
-	filePath := "../data/kargerMinCut.txt"
-	vertices, edges := graph.ConstructGraph(filePath)
+	balance := make([]int, 0)
 
-	minOfMincut := 100000 // larger than 2517 :P
-	quitCh := make(chan bool)
-
-	go report(&minOfMincut, quitCh)
-
-	for i := 0; i < 50000; i++ { // n := len(vertices) // lgn := int(math.Log2(float64(n))) // for i := 0; i < n*n*lgn; i++ {
-		vertices, edges = graph.ConstructGraph(filePath)
-
-		mincut := graph.RContraction(vertices, edges)
-
-		if mincut < minOfMincut {
-			minOfMincut = mincut
+	for _, v := range debt {
+		if v != 0 {
+			balance = append(balance, v)
 		}
 	}
 
-	fmt.Print("min-cut is ")
-	fmt.Println(minOfMincut)
-
-	quitCh <- true
+	return dfs(0, balance)
 }
 
-var r = rand.New(rand.NewSource(time.Now().Unix()))
+func dfs(i int, balance []int) int {
+	if i == len(balance) {
+		return 0
+	}
+
+	if balance[i] == 0 {
+		return dfs(i+1, balance)
+	}
+
+	minTrans := maxInt
+
+	for j := i + 1; j < len(balance); j++ {
+		if balance[i]*balance[j] < 0 {
+			temp := balance[j]
+
+			balance[j] += balance[i]
+			minTrans = min(minTrans, 1+dfs(i+1, balance))
+			balance[j] = temp
+
+			if balance[i]+balance[j] == 0 {
+				break
+			}
+		}
+	}
+
+	return minTrans
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+const maxUint = ^uint(0)         // 1111...1
+const minUint = uint(0)          // 0000...0
+const maxInt = int(maxUint >> 1) // 0111...1
+const minInt = -maxInt - 1       // 1000...0
 
 func main() {
-	fmt.Println("hello world")
+	balance := []int{-8, -6, -5, -4, -2, -1, -1, 2, 3, 3, 3, 4, 6, 6}
+
+	fmt.Println(dfs(0, balance))
 }
